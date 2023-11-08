dd7976c1b459a94811da927358e1bf243c838802
cbrizzy003@gmail.com <cbrizzy003@gmail.com>
2023-11-08 16:17:18 -0600

    Added showEstablised method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -18,7 +18,9 @@ function show(data) {
                             <h2>Ratings</h2>
                             <p>Not Rated</p>
                             <h2>Description</h2>
-                            <p>Located in {`${data.place.city}`}, {`${data.place.state}`} and serving {`${data.place.cuisines}`}</p>
+                            <h3>{data.place.showEstablished()}</h3>
+                            <h4>Serving {data.place.cuisines}</h4>
+                            <h3>Located in {`${data.place.city}`}, {`${data.place.state}`}</h3>
                             <div className='edit-btns'>
                                 <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
                                 <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
