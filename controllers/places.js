f3372a3848634c76a7b3df2d5567f68bdbc4bc63
cbrizzy003@gmail.com <cbrizzy003@gmail.com>
2023-11-10 05:22:22 -0600

    Added a function to add new comments

--- controllers/places.js
+++ controllers/places.js
@@ -3,13 +3,12 @@ const db = require('../models')
 
 router.get('/', (req, res) => {
     db.Place.find()
-    .then((places) => {
-        res.render('places/index', {places})
-    })
-    .catch (err => {
-        console.log(err)
-        res.render('error404.jsx')
-    })
+        .then((places) => {
+            res.render('places/index', { places })
+        })
+        .catch(err => {
+            res.render('error404.jsx')
+        })
 })
 
 router.post('/', (req, res) => {
@@ -19,22 +18,22 @@ router.post('/', (req, res) => {
     }
 
     db.Place.create(req.body)
-    .then(() => {
-        res.redirect('/places')
-    })
-    .catch (err => {
-        if (err && err.name == 'ValidationError' ) {
-            let message = 'Validation Error: '
-            for (let field in err.errors) {
-                message += `${field} was ${err.errors[field].value} `
-                message += `${err.errors[field].message}`
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: '
+                for (let field in err.errors) {
+                    message += `${field} was ${err.errors[field].value} `
+                    message += `${err.errors[field].message}`
+                }
+                // TODO: Generate error message (s)
+                res.render('places/new', { message })
+            } else {
+                res.render('error404')
             }
-            // TODO: Generate error message (s)
-            res.render('places/new', { message })
-        } else {
-            res.render('error404')
-        }
-    })
+        })
 })
 
 router.get('/new', (req, res) => {
@@ -43,13 +42,36 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
-    .then(place => {
-        res.render('places/show', { place })
-    })
-    .catch(err => {
-        console.log(err)
-        res.render('error404')
-    })
+        .populate('comments')
+        .then(place => {
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
+
+router.post('/:id/comment', (req, res) => {
+    req.body.rant = req.body.rant ? true : false
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    res.status(404).render('error404')
+                })
+        })
+        .catch(err => {
+            res.status(404).render('error404')
+        })
+    console.log('req.params.id:', req.params.id);
+    console.log('req.body:', req.body);
 })
 
 router.put('/:id', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,23 @@ const React = require('react')
 const Def = require('../default.jsx')
 
 function show(data) {
+    let comments = (
+        <h3 className='inactive'>No comments yet!</h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Ratings: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <head>
@@ -16,7 +33,7 @@ function show(data) {
                         <div className='show-info'>
                             <h1>{data.place.name}</h1>
                             <h2>Ratings</h2>
-                            <p>Not Rated</p>
+                            <h3>not yet rated</h3>
                             <h2>Description</h2>
                             <h3>{data.place.showEstablished()}</h3>
                             <h4>Serving {data.place.cuisines}</h4>
@@ -29,8 +46,32 @@ function show(data) {
                             </div>
                         </div>
                     </div>
+                    <h2>Comments</h2>
                     <div className='show-comments'>
-                        <h2>Comments</h2>
+                        {comments}
+                    </div>
+                    <div className='review-input'>
+                        <form method='POST' action={`/places/${data.place.id}/comment`}>
+                            <h4>
+                                <label htmlFor="content">Content</label><br />
+                            </h4>
+                            <textarea name="content" id="content" cols="100" rows="5" required></textarea>
+                            <div className='review-input2'>
+                                <h4>
+                                    <label htmlFor="author">Author</label>
+                                </h4>
+                                <input className='author-input' type="text" id='author' name='author' />
+                                <h4>
+                                    <label htmlFor="stars">Star Ratings</label>
+                                </h4>
+                                <input type="range" id='stars' name='stars' min={1} max={5} />
+                                <h4>
+                                    <label htmlFor="rant">Rant!</label>
+                                </h4>
+                                <input type="checkbox" id='rant' name='rant' />
+                            </div>
+                            <input type="submit" />
+                        </form>
                     </div>
                 </div>
             </main>
